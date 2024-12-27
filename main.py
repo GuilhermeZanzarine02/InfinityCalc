f0e42d370a6421388ab67cdb51efefc9b3fe4b5d
Guilherme Zanzarine <guizanzarin2002@gmail.com>
2024-12-27 15:15:58 -0300

    Adding new features

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-print("teste 2.0")
+from flask import Flask, render_template, request, flash
+
+app = Flask(__name__)
+
+# Rota para a página inicial
+@app.route('/', methods=['GET', 'POST'])
+
+def index():
+    if request.method == 'POST':
+        
+        flash('Formulário enviado com sucesso!')
+        return render_template('Homepage/index.html') 
+
+    return render_template('Homepage/index.html')  
+
+# Rota para a página de juros simples (exemplo)
+@app.route('/simpleInterestPage', methods=['GET', 'POST'])
+
+def simple_interest_page():
+    return render_template('simpleInterestPage/simpleInterest.html') 
+
+if __name__ == '__main__':
+    app.run(debug=True)
